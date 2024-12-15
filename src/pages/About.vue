c4cd49a523bb53290c5b8b78e0b9b5af3599e5ab
babaphillips <amandavphillips@icloud.com>
2024-12-15 12:06:57 -0500

    More work

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,1 +1,25 @@
-<template><h1>About</h1></template>
+<template>
+  <v-card class="bg-white text-center" flat
+    ><v-card-title class="text-h2">JRW History</v-card-title
+    ><v-card-text
+      >Stephanie H. Edwards (Stephie-Jo) formed James River Women’s Rugby
+      Football Club (JWRFC) in the Spring of 2001. Before that, there was not a
+      women’s club team in Richmond, VA. With the assistance of the James River
+      Men’s and United Men’s RFC, JRWRFC was able to gather interested players
+      from the area. Enough players came out to establish the need to schedule
+      practices. With the enthusiasm of these new players, James River entered a
+      team in the Cape Fear Sevens Tournament that summer. There was a positive
+      reaction from the players to continue with a team in the fall. James River
+      Men’s RFC, our established brother club, allowed us to use a portion of
+      their field and assisted in coaching the new players during the fall
+      season. Schedule practices began in mid-August and continued through the
+      first part of November. Eight scheduled matches were played and several
+      scrimmages with Virginia Commonwealth University Women’s Football Club
+      were organized and executed. The team also participated in the Sandy Lee
+      State Cup in late October. After a successful trial run, the women decided
+      to move forward with the club. On January 15, 2002, the club held its
+      inaugural Annual General Meeting (AGM). The club voted in Shannon Plum as
+      the newly elected president; as well as, its first executive board. </v-card-text
+    ><span>Bleed Blue and Green! </span></v-card
+  >
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,41 +1,27 @@
 <template>
-  <v-card class="mx-auto bg-white border elevation-0" hover>
-    <v-card-item>
-      <v-card-title class="text-center">
-        Welcome To The Home Of James River Women's Rugby Football Club (JRWRFC)
-      </v-card-title>
+  <v-card flat class="ma-12">
+    <v-row>
+      <v-col cols="12" class="bg-white"
+        ><span class="text-h6 text-green-darken-3">
+          We embrace those who are knowledgeable or brand new to rugby. If you
+          are interested in developing your skills or learning the first thing
+          about rugby, we would love to meet you. As a team, we share a great
+          camaraderie and love for this sport so we hope our enthusiasm is
+          equally infectious. Please make plans to come out and introduce
+          yourself at our practices, matches, and/or socials. All personalities,
+          athletic conditions, and skill levels welcome. ​ Thank you for taking
+          the time to visit our site!</span
+        ></v-col
+      ><v-col cols="12" class="bg-white"
+        ><v-carousel hide-delimiters>
+          <v-carousel-item :src="fallSchedule2024"></v-carousel-item>
 
-      <v-card-subtitle class="text-center">
-        No experience necessary!</v-card-subtitle
-      >
-    </v-card-item>
-
-    <v-card-text>
-      <v-row>
-        <v-col cols="6">
-          <v-img
-            :src="fallSchedule2024"
-            max-height="100%"
-            max-width="100%"
+          <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
             cover
-          ></v-img
-        ></v-col>
-        <v-col cols="6"
-          ><span>
-            We embrace those who are knowledgeable or brand new to rugby. If you
-            are interested in developing your skills or learning the first thing
-            about rugby, we would love to meet you. As a team, we share a great
-            camaraderie and love for this sport so we hope our enthusiasm is
-            equally infectious. Please make plans to come out and introduce
-            yourself at our practices, matches, and/or socials. All
-            personalities, athletic conditions, and skill levels welcome. ​
-            Thank you for taking the time to visit our site!</span
-          ></v-col
-        >
-      </v-row>
-
-      <span></span>
-    </v-card-text>
+          ></v-carousel-item> </v-carousel
+      ></v-col>
+    </v-row>
   </v-card>
 </template>
 
